20956f66f5ae00a155cf4e923feaeb7b835d39f4
chenshuisenlin <csslneuq@163.com>
2023-02-27 14:27:46 +0800

    开发1提交

---
+++ src/main/java/com/example/projectmq/VO/ViewObject1.java
@@ -0,0 +1,15 @@
+package com.example.projectmq.VO;
+
+import lombok.Data;
+
+/**
+ * @ClassName ViewObject1
+ * @Description 开发视图1
+ * @Author cssl
+ * @DATE 2023/2/27 14:25
+ * @Version 1.0
+ */
+@Data
+public class ViewObject1 {
+    private String name;
+}
